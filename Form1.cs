915ee785d987c5fea59d544ed78fa237cf69d72c
eliaschen <elias9988110@outlook.com>
2025-01-25 09:44:45 +0800

    add comment

--- Form1.cs
+++ Form1.cs
@@ -16,12 +16,14 @@ namespace ooxx
 
         private void Form1_Load(object sender, EventArgs e)
         {
-            pointSize = 100;
-            gap = 10;
-            totalWidth = (pointSize * 3) + (gap * (3 - 1));
-            totalHeight = (pointSize * 3) + (gap * (3 - 1));
-            startX = (this.ClientSize.Width / 2) - (totalWidth / 2);
-            startY = 10;
+
+            pointSize = 100; // 每個格子的大小
+            gap = 10; // 格子之間的間距
+            totalWidth = (pointSize * 3) + (gap * (3 - 1)); // 棋盤的寬度
+            totalHeight = (pointSize * 3) + (gap * (3 - 1)); // 棋盤的高度
+            startX = (this.ClientSize.Width / 2) - (totalWidth / 2); // 棋盤的起始X座標
+            startY = 10; // 棋盤的起始Y座標
+            // 用來顯示picturebox
             for (int i = 0; i < 3; i++)
             {
                 for (int j = 0; j < 3; j++)
@@ -40,56 +42,59 @@ namespace ooxx
                     playPoint.Click += new System.EventHandler(this.playPoint_Click);
                 }
             }
+            // 崁入事件function
             Share.statusUpdated += statusUpdated;
             Share.gameEnd += gameEnd;
+            // npc(電腦)先下
             npcPlay();
         }
-        private void gameEnd()
+        private void gameEnd() // 遊戲結束
         {
+            // 顯示繼續玩的按鈕
             Button restart = new Button();
             restart.Text = "Play Again";
             restart.Location = new Point((this.ClientSize.Width / 2) - (restart.Width / 2), this.ClientSize.Height - restart.Height - 10);
             restart.Click += new EventHandler(restart_Click);
             this.Controls.Add(restart);
         }
-        void restart_Click(object sender, EventArgs e)
+        void restart_Click(object sender, EventArgs e) // 重新開始遊戲按鈕
         {
             Button restart = (Button)sender;
             this.Controls.Remove(restart);
             Share.reset();
             npcPlay();
         }
-        private void checkRules()
+        private void checkRules() // 檢查整個棋盤是否有人贏
         {
-            for (int i = 0; i < 8; i++)
+            for (int i = 0; i < 8; i++) // 有8條連成一條線的連線
             {
-
-                if (!Share.hasChecked.Contains(i))
+                if (!Share.hasChecked.Contains(i)) // 如果這條連線還沒檢查過
                 {
-                    int[] line = new int[3];
+                    int[] line = new int[3]; // 連成一條線的3個格子
                     for (int j = 0; j < 3; j++)
                     {
                         line[j] = Share.get(Share.rules[i, j, 0], Share.rules[i, j, 1]);
                     }
-                    if (line[0] == line[1] && line[1] == line[2])
+                    if (line[0] == line[1] && line[1] == line[2]) // 如果這條連線有連成一條線
                     {
-                        if (line[0] == 0)
+                        if (line[0] == 0) // 如果是玩家贏了
                         {
                             Share.userScore += 1;
-                            Share.hasChecked.Add(i);
+                            Share.hasChecked.Add(i); // 把這條連線加入已經檢查過的連線
                         }
-                        else if (line[0] == 1)
+                        else if (line[0] == 1) // 如果是npc贏了
                         {
                             Share.npcScore += 1;
-                            Share.hasChecked.Add(i);
+                            Share.hasChecked.Add(i); // 把這條連線加入已經檢查過的連線
                         }
+                        // 更新計分板label分數
                         npcScore.Text = Share.npcScore.ToString();
                         userScore.Text = Share.userScore.ToString();
                     }
                 }
             }
         }
-        private void Form1_Paint(object sender, PaintEventArgs e)
+        private void Form1_Paint(object sender, PaintEventArgs e) // 畫出棋盤
         {
             using (Pen pen = new Pen(Color.Black, 5))
             {
@@ -107,7 +112,7 @@ namespace ooxx
         }
 
 
-        private void statusUpdated()
+        private void statusUpdated() // 更新棋盤
         {
             for (int i = 0; i < 3; i++)
             {
@@ -131,13 +136,13 @@ namespace ooxx
         }
         private void playPoint_Click(object sender, System.EventArgs e)
         {
-            PictureBox playPoint = (PictureBox)sender;
+            PictureBox playPoint = (PictureBox)sender; // 取得被點擊的PictureBox資料
             var first = int.Parse(playPoint.Name[0].ToString());
             var second = int.Parse(playPoint.Name[1].ToString());
 
-            if (Share.get(first, second) == 2)
+            if (Share.get(first, second) == 2) // 如果該格子是空的
             {
-                Share.set(first, second, 0);
+                Share.set(first, second, 0); // 設定該格子為玩家
                 npcPlay();
                 checkRules();
             }
@@ -149,12 +154,13 @@ namespace ooxx
             {
                 int first = random.Next(0, 3);
                 int second = random.Next(0, 3);
-                if (Share.get(first, second) == 2)
+                if (Share.get(first, second) == 2) // 如果該格子是空的
                 {
-                    Share.set(first, second, 1);
+                    Share.set(first, second, 1); // 設定該格子為npc
                     break;
                 }
             }
+            // 檢查整個棋盤是否有人贏
             checkRules();
         }
     }
--- Share.cs
+++ Share.cs
@@ -6,20 +6,20 @@ namespace ooxx
 {
     internal class Share
     {
-        public static event Action gameEnd;
-        public static event Action statusUpdated;
-        public static int userScore = 0;
-        public static int npcScore = 0;
-        public static List<int> hasChecked = new List<int>();
+        public static event Action gameEnd; // 遊戲結束事件
+        public static event Action statusUpdated; // 更新畫面事件
+        public static int userScore = 0; // 玩家分數
+        public static int npcScore = 0; // npc分數
+        public static List<int> hasChecked = new List<int>(); // 已經檢查過的連線
         private static int[,] _defaultStatus =
         {
                 { 2, 2, 2 },
                 { 2, 2, 2 },
                 { 2, 2, 2 }
             };
-        private static int[,] _status = (int[,])_defaultStatus.Clone();
-        public static int times = 0;
-        public static int[,,] rules =
+        private static int[,] _status = (int[,])_defaultStatus.Clone(); // 棋盤的狀態
+        public static int times = 0; // 下了幾次(每次遊戲)
+        public static int[,,] rules = // 設定連線的規則
             {
                 {{0,0},{0,1},{0,2}},
                 {{1,0},{1,1},{1,2}},
@@ -30,23 +30,23 @@ namespace ooxx
                 {{0,0},{1,1},{2,2}},
                 {{0,2},{1,1},{2,0}}
             };
-        public static int get(int x, int y)
+        public static int get(int x, int y) // 取得格子的值
         {
-            return _status[x, y];
+            return _status[x, y]; 
         }
-        public static void reset()
+        public static void reset() // 重置遊戲
         {
-            _status = (int[,])_defaultStatus.Clone();
-            times = 0;
-            hasChecked.Clear();
-            statusUpdated?.Invoke();
+            _status = (int[,])_defaultStatus.Clone(); // 複製一份新的陣列(一定要這樣做，不然會資料連動)
+            times = 0; // 重置次數
+            hasChecked.Clear(); // 清空已經檢查過的連線
+            statusUpdated?.Invoke(); // 觸發更新畫面事件
         }
-        public static void set(int x, int y, int value)
+        public static void set(int x, int y, int value) // 設定格子的值
         {
             _status[x, y] = value;
-            times++;
-            if (times == 9) gameEnd?.Invoke();
-            statusUpdated?.Invoke();
+            times++; 
+            if (times == 9) gameEnd?.Invoke(); // 觸發遊戲結束事件 npc和user都下到不能下了 棋盤總共9格
+            statusUpdated?.Invoke(); // 觸發更新畫面事件
         }
     }
 }
